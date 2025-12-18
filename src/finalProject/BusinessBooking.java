4acf2dd71214f9c00f1fde7a985eb221644b734f
Samasoo <24100408@usc.edu.ph>
2025-12-18 23:55:57 +0800

    Update BusinessBooking.java

--- src/finalProject/BusinessBooking.java
+++ src/finalProject/BusinessBooking.java
@@ -5,9 +5,6 @@ import java.util.Date;
 public class BusinessBooking extends Booking {
     // Inheritance
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public BusinessBooking(User bookedBy, String destinationCity, String flightDetails, Date travelDate) {
