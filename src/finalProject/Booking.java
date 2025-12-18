cbd36466d704efe00104f380ef6ee67a05508860
ber <amberlerio28@gmail.com>
2025-12-18 23:34:13 +0800

    Implement getSeatClass method in Booking class
    Added getSeatClass method to Booking class.

--- src/finalProject/Booking.java
+++ src/finalProject/Booking.java
@@ -77,6 +77,10 @@ public class Booking implements Serializable {
         return travelDate.after(new Date()) ? "Upcoming" : "Past";
     }
 }
+	public String getSeatClass() {
+		return null;
+	}
+}
 
 /*
  * ===================== ABSTRACTION =====================
